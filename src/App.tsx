c6476d244702a96b786f60c2c9fa1b46b6dccc10
Stafford Williams <stafford.williams@gmail.com>
2021-09-04 16:55:32 +1000

    remove react stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { makeStyles } from "@material-ui/core";
 import { format } from "prettier/standalone";
@@ -30,50 +29,26 @@ function App() {
     };
     load();
   }, []);
+
+  const formatAndRun = async () => {
+    if (sandbox) {
+      const source = sandbox.getText();
+      const formatted = format(source, {
+        parser: "typescript",
+        plugins: [parserTypescript],
+      });
+      sandbox.setText(formatted);
+      eval(await sandbox.getRunnableJS());
+    }
+  };
+
   return (
     <div className={classes.app}>
       <header className={classes.header}>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
         <p>
-          <button
-            type="button"
-            onClick={async () => {
-              if (sandbox) {
-                const source = sandbox.getText();
-                const formatted = format(source, {
-                  parser: "typescript",
-                  plugins: [parserTypescript],
-                });
-                sandbox.setText(formatted);
-                eval(await sandbox.getRunnableJS());
-              }
-            }}
-          >
+          <button type="button" onClick={formatAndRun}>
             Run
           </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates/.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
         </p>
       </header>
       <div id="monaco-editor-embed" className={classes.editor}></div>
